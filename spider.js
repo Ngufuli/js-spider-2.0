a834d2f6eab0f53ac1fbb4dbed27fdc0c3252943
Ngufuli <ngufuli@outlook.com>
2023-01-17 15:43:06 +0530

    sequential crawling of links

--- spider.js
+++ spider.js
@@ -17,3 +17,28 @@ export function spider(url, nesting, cb){
         spiderLinks(url, fileContent, nesting, cb)
     })
 }
+
+function spiderLinks(currentUrl, body, nesting, cb){
+    if(nesting === 0){
+        return ProcessingInstruction.nextTick(cb);
+    }
+
+    const links = getPageLinks(currentUrl, body)
+    if(links.length === 0){
+        return ProcessingInstruction.nextTick(cb);
+    }
+
+    function iterate(index){
+        if(index === links.length){
+            return cb()
+        }
+
+        spider(links[index], nesting - 1, function(err){
+            if(err){
+                return cb(err);
+            }
+            iterate(index + 1)
+        })
+    }
+    eterate(0);
+}
