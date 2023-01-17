633f10ca8c361b17c46c1efbc53af80c29977978
Ngufuli <ngufuli@outlook.com>
2023-01-17 16:15:24 +0530

    added spider client

---
+++ spider-cli.js
@@ -0,0 +1,12 @@
+import {spider} from "./spider";
+
+const url = process.argv[2];
+const nesing = Number.parseInt(process.argv[3],10)||1;
+
+spider(url, nesting, err=>{
+    if(err){
+        console.error(err);
+        process.exit(1);
+    }
+    console.log(`Download complete`);
+})
